a72539f4d52cfb3a0f0f4c2721d9ae386adec7ae
virusv2 <livintodie4e@gmail.com>
2022-11-14 22:57:48 -0500

    added item todo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Python Script that will help you decide what to make for dinner for the week"""
 # standard imports
 # TODO: add save functionality for weekly recipes
+# TODO: add option to get fast food x times a week for dinner
 
 from modules.functions import console, load_json, save_json, evaluate_recipes, choose_recipes, combine_ingredients
 
