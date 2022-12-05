bd91f6d77b7dc08b37009cd8fa49bfbb58949a63
drew-k <drewkinney11@yahoo.com>
2022-12-05 00:38:14 -0500

    Add docstrings

--- models/recipe.py
+++ models/recipe.py
@@ -1,16 +1,21 @@
+""" Recipe class """
+
 from dataclasses import dataclass
 import datetime
 
 @dataclass
 class Recipe:
+    """ Dataclass for recipe objects """
 
-    name: str
-    ingredients: dict
-    last_cooked: datetime = None
-    fast_food: bool = False
+    name: str  # Name of the recipe
+    ingredients: dict  # Dictionary of ingredients
+    last_cooked: datetime = None  # Last cooked date, defaults to never cooked
+    fast_food: bool = False  # Fast food flag, defaults to False
 
     def __getitem__(self, key):
+        """ Attr getter """
         return getattr(self, key)
-    
+
     def __setitem__(self, key, value):
+        """ Attr setter """
         return setattr(self, key, value)
